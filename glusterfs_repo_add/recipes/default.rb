d10a98c7546643a7e19a02135031bb9eb529b539
javadshareefma <javadshareefma@gmail.com>
2015-10-01 19:04:46 +0530

    test commit

--- glusterfs_repo_add/recipes/default.rb
+++ glusterfs_repo_add/recipes/default.rb
@@ -2,3 +2,39 @@ cookbook_file "/etc/yum.repos.d/glusterfs.repo" do
   source "glusterfs.repo"
   action :create
 end
+
+yum_package 'glusterfs-fuse' do
+  action :install
+  flush_cache [ :before ]
+end
+yum_package 'nfs-utils' do
+  action :install
+  flush_cache [ :before ]
+end
+yum_package 'glusterfs-fuse' do
+  action :install
+  flush_cache [ :before ]
+end
+
+yum_package 'nginx' do
+  action :install
+  flush_cache [ :before ]
+end
+
+yum_package 'php-fpm' do
+  action :install
+  flush_cache [ :before ]
+end
+
+yum_package 'php' do
+  action :install
+  flush_cache [ :before ]
+end
+
+service "nginx" do
+  action :enable
+end
+
+service "php-fpm" do
+  action :enable
+end
