4391a9e39b14208e8ff7e49c45909166fa7dd727
javadshareefma <javadshareefma@gmail.com>
2015-10-01 20:27:12 +0530

    update

--- nginx_config/recipes/default.rb
+++ nginx_config/recipes/default.rb
@@ -3,21 +3,21 @@ cookbook_file "/etc/nginx/nginx.conf" do
   action :create
 end
 
-script 'mount_fs' do
-  interpreter "bash"
-  cwd ::File.dirname(src_filepath)
+bash "mount_fs" do
+  cwd Chef::Config[:file_cache_path]
   code <<-EOH
     mkdir -p /home/webs/
     echo "ip-172-31-10-61:webs /home/webs glusterfs defaults,_netdev 0 0" >> /etc/fstab
     mount -a
-    EOH
-  not_if { ::File.exists?(extract_path) }
+  EOH
+  creates "/usr/local/bin/redis-server"
 end
 
+
 service "nginx" do
-  action :start
+  action :restart
 end
 
 service "php-fpm" do
-  action :start
+  action :restart
 end
